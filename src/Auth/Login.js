1824389bece6cf273de222304b1cb95849de50aa
SAIRAM <87373136+B-Sairam@users.noreply.github.com>
2022-03-21 12:17:06 +0530

    Update Login.js

--- src/Auth/Login.js
+++ src/Auth/Login.js
@@ -30,7 +30,7 @@ const loginHandler=async()=>{
   if(!email||!password){
     setAlertbg(false)
     handleVisible()
-    setMessage('Ellataium Fill pannuga da')
+    setMessage('Ellataium Fill pannuga')
     setLoading(false)
     // return;
   }else{
@@ -51,7 +51,7 @@ const loginHandler=async()=>{
     } catch (error) {
       setAlertbg(false)
       handleVisible()
-      setMessage('Ollunga Passwordaium emailaium poodugada')
+      setMessage('Ollunga Passwordaium emailaium pooduga')
       setLoading(false)
     }
   }
